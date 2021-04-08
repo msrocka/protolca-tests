1cfb04f4472d680cfb7c6329320f858c4f44a7b7
msrocka <michael.srocka@gmail.com>
2021-04-08 18:39:18 +0200

    load config but typing does not work yet

--- config.d.ts
+++ config.d.ts
@@ -1,35 +1,41 @@
-export interface Ref {
-  type?: string;
-  id?: string;
-  name?: string;
-  refUnit?: string;
-  location?: string;
-}
+declare module 'config' {
 
-export interface Result {
-  id: string;
-}
+  export interface Ref {
+    type?: string;
+    id?: string;
+    name?: string;
+    refUnit?: string;
+    location?: string;
+  }
 
-export interface ImpactFactorRequest {
-  result: Result;
-  indicator?: Ref;
-  flow?: Ref;
-}
+  export interface Result {
+    id: string;
+  }
 
-export interface ImpactFactorResponse {
-  indicator: Ref;
-  flow: Ref;
-  value: number;
-}
+  export interface ImpactFactorRequest {
+    result: Result;
+    indicator?: Ref;
+    flow?: Ref;
+  }
 
-export interface ResultService {
-  getImpactFactors: (req: ImpactFactorRequest) =>
-    StreamResponse<ImpactFactorResponse>;
-}
+  export interface ImpactFactorResponse {
+    indicator: Ref;
+    flow: Ref;
+    value: number;
+  }
 
-export interface StreamResponse<T> {
-  on: (event: 'data', fn: (datum: T) => void) => void;
-  on: (event: 'error', fn: (err: any) => void) => void;
-  on: (event: 'end', fn: () => void) => void;
-  on: (event: 'status', fn: (status: any) => void) => void;
+  export interface ResultService {
+    getImpactFactors: (req: ImpactFactorRequest) =>
+      StreamResponse<ImpactFactorResponse>;
+  }
+
+  export interface StreamResponse<T> {
+    on: (event: 'data', fn: (datum: T) => void) => void;
+    on: (event: 'error', fn: (err: any) => void) => void;
+    on: (event: 'end', fn: () => void) => void;
+    on: (event: 'status', fn: (status: any) => void) => void;
+  }
+
+  export function getResultService(): ResultService;
+
 }
--- config.js
+++ config.js
@@ -3,29 +3,25 @@ const grpc = require('@grpc/grpc-js')
 
 const URL = 'localhost:8080'
 
-/** @type import('./config').Ref */
 const EXAMPLE_SYSTEM_REF = {
   id: '66344e38-411a-4996-8062-e92dd3cd211c'
 }
 
-
-
 const protoDir = `${__dirname}/protos`
 
-/** @type import('./config').ResultService */
 let _resultService = null;
-export function getResultService() {
+exports.getResultService = () => {
   if (_resultService) {
     return _resultService
   }
   const p = loadProto('results')
-  _resultService = p.protolca.services.ResultService(
+  _resultService = new p.protolca.services.ResultService(
     URL, grpc.credentials.createInsecure())
   return _resultService
 }
 
 function loadProto(proto) {
-  const fullPath = `${__dirname}/protos/${proto}.protos`
+  const fullPath = `${__dirname}/protos/${proto}.proto`
   const protos = loader.loadSync(fullPath, { enums: String })
   return grpc.loadPackageDefinition(protos)
 }
