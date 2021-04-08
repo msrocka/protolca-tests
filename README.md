96ca468cc0084cdc2a9a1384b9fa5d6fb0b6cd8b
msrocka <michael.srocka@gmail.com>
2021-04-08 17:28:13 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# protolca-tests
+This is a test suite for the gRPC service interface of openLCA.
+
+```bash
+npm install
+npx mocha
+```
---
+++ test/result_impact_factors.js
@@ -0,0 +1,7 @@
+const assert = require('assert')
+
+describe('Get impact factors from results', () => {
+  it('should return impact factors for an indicator', () => {
+    assert.strictEqual('a', 'b')
+  })
+})
